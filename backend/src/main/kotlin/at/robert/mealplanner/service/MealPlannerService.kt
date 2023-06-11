dab61efcac2c1d1cbde0a1f28ec2e684ea1d0019
Robert Test <robert.tieber@gmail.com>
2023-06-11 21:09:16 +0200

    Add MealPlannerService stub

---
+++ backend/src/main/kotlin/at/robert/mealplanner/service/MealPlannerService.kt
@@ -0,0 +1,12 @@
+package at.robert.mealplanner.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class MealPlannerService {
+
+    fun calculateMealPlan() {
+        return TODO()
+    }
+
+}
