174435ed609f113e8a0b2ef2f7c57dbc27de63c9
Robert Tieber <robert.tieber@gmail.com>
2023-11-18 12:39:41 +0100

    Use PlannedMeal

--- frontend/src/planner/MealPlanner.tsx
+++ frontend/src/planner/MealPlanner.tsx
@@ -3,24 +3,20 @@ import RCalendar from "../lib/RCalendar";
 import {DateTime} from "luxon";
 import {Col} from "react-bootstrap";
 import "./MealPlanner.css";
-import {PlannedMealControllerService} from "../generated";
-
-interface Meal {
-    name: string;
-}
+import {PlannedMeal, PlannedMealControllerService} from "../generated";
 
 const MealPlanner: React.FC = () => {
     const dayCount = 7;
 
-    const [meals, setMeals] = React.useState<Meal[][]>(Array.from({length: dayCount}, () => []));
-    const addMeal = (day: number, newMeal: Meal) => {
+    const [meals, setMeals] = React.useState<PlannedMeal[][]>(Array.from({length: dayCount}, () => []));
+    const addMeal = (day: number, newMeal: PlannedMeal) => {
         if (day < 0 || day >= dayCount) throw new Error("Invalid day");
 
         meals[day].push(newMeal);
         setMeals([...meals]);
     };
 
-    const updateMeal = (day: number, mealIndex: number, updatedMeal: Meal) => {
+    const updateMeal = (day: number, mealIndex: number, updatedMeal: PlannedMeal) => {
         if (day < 0 || day >= dayCount) throw new Error("Invalid day");
 
         setMeals((prevMeals) => {
@@ -46,9 +42,7 @@ const MealPlanner: React.FC = () => {
         meals.then((meals) => {
             setMeals((prevMeals) => {
                 const newMeals = [...prevMeals];
-                newMeals[day.diff(DateTime.now(), 'days').days] = meals.map((meal) => {
-                    return {name: meal.data.desiredCalories?.toString() || "Undefined"};
-                });
+                newMeals[day.diff(DateTime.now(), 'days').days] = meals;
                 return newMeals;
             });
         });
@@ -70,12 +64,12 @@ const MealPlanner: React.FC = () => {
                             key={i}
                             meals={meals[i]}
                             addMeal={
-                                function (meal: Meal) {
+                                function (meal: PlannedMeal) {
                                     addMeal(i, meal);
                                 }
                             }
                             updateMeal={
-                                function (mealIndex: number, meal: Meal) {
+                                function (mealIndex: number, meal: PlannedMeal) {
                                     updateMeal(i, mealIndex, meal);
                                 }
                             }
@@ -93,21 +87,21 @@ const MealPlanner: React.FC = () => {
 }
 
 interface DayContentProps {
-    meals: Meal[];
-    addMeal: (meal: Meal) => void;
-    updateMeal: (mealIndex: number, meal: Meal) => void;
+    meals: PlannedMeal[];
+    addMeal: (meal: PlannedMeal) => void;
+    updateMeal: (mealIndex: number, meal: PlannedMeal) => void;
     removeMeal: (mealIndex: number) => void;
 }
 
 const DayContent: React.FC<DayContentProps> = (props) => {
     const addNewMeal: MouseEventHandler<HTMLDivElement> = (e) => {
-        props.addMeal({name: "New meal"});
+        //     props.addMeal({day});
     }
 
     return <Col>
         {
             props.meals.map((meal, i) => {
-                return <div key={i}>{meal.name}</div>
+                return <div key={i}>{meal.id}</div>
             })
         }
         <div className="add-new-meal" onClick={addNewMeal}>Add new meal</div>
